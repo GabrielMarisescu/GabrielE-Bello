6ed32daba81a57113c4c5257fba266e378f24b92
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2024-01-31 19:47:26 +0100

    Added mongoose package and integrated mongodb, create users and comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://Equinox10:KIZutetirJGEzHvt@cluster0.nsvsqxp.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
